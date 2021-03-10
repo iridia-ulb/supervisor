410a7ee42c1a7fa3bebc0d5acfaf88206180494f
Michael Allwright <allsey87@gmail.com>
2021-03-10 17:00:41 +0100

    Open the webbrowser on start-up

--- src/main.rs
+++ src/main.rs
@@ -20,45 +20,31 @@ mod router;
 /// 1c. Start experiment triggers upload to all robots? [DONE]
 /// 1d. Add basic control software checks, is there one .argos file, are all referenced files included?
 /// 1e. Start ARGoS with the controller and shutdown at end of experiment 
-/// 2. Add the ping functionality to remove robots if they don't reply
-///    a. What if SSH drops from drone, but Xbee is still up? (move back to the standby state?)
-///    b. What if Xbee drops, but SSH is still up? (these are difficult problems to solve)
-/// 3. Investigate the SSH shell drop outs (and add fallback code to tolerate this)
-/// 4. Add code to catch SIGINT
 
 #[tokio::main]
 async fn main() {
     /* initialize the logger */
     let environment = env_logger::Env::default().default_filter_or("mns_supervisor=info");
     env_logger::Builder::from_env(environment).init();
-    
     /* create a task for tracking the robots and state of the experiment */
     let (arena_requests_tx, arena_requests_rx) = mpsc::unbounded_channel();
     let (network_addr_tx, network_addr_rx) = mpsc::unbounded_channel();
     let (journal_requests_tx, journal_requests_rx) = mpsc::unbounded_channel();
-    
     /* add all network addresses from the 192.168.1.0/24 subnet */
     for network_addr in "192.168.1.0/24".parse::<Ipv4Net>().unwrap().hosts() {
         network_addr_tx.send(network_addr).unwrap();
     }
     let message_router_addr : SocketAddr = ([127, 0, 0, 1], 4950).into();
-
     /* listen for the ctrl-c shutdown signal */
     let sigint_task = tokio::signal::ctrl_c();
-
     /* create journal task */
     let journal_task = journal::new(journal_requests_rx);
-
     /* create arena task */
     let arena_task = arena::new(message_router_addr, arena_requests_rx, &network_addr_tx, &journal_requests_tx);
-
     /* create network task */
     let network_task = network::new(network_addr_rx, &arena_requests_tx);
-    
     /* create message router task */
-    let router_socket_addr : SocketAddr = ([127, 0, 0, 1], 4950).into();
-    let router_task = router::new(router_socket_addr, &journal_requests_tx);
-
+    let router_task = router::new(message_router_addr, &journal_requests_tx);
     /* create webui task */
     /* clone arena requests tx for moving into the closure */
     let arena_requests_tx = arena_requests_tx.clone();
@@ -74,15 +60,21 @@ async fn main() {
         .and(warp::fs::dir("/home/mallwright/Workspace/mns-supervisor/static"));
     let server_addr : SocketAddr = ([127, 0, 0, 1], 3030).into();
     let webui_task = warp::serve(socket_route.or(static_route)).run(server_addr);
-
-    /* pin the futures so that they can be polled */
+    /* pin the futures so that they can be polled via &mut */
     tokio::pin!(arena_task);
     tokio::pin!(journal_task);
     tokio::pin!(network_task);
     tokio::pin!(router_task);
     tokio::pin!(webui_task);
     tokio::pin!(sigint_task);
-
+    /* open a local browser window */
+    /* TODO: implement "reconnecting in javascript", if no client connects within 1 second,
+       open new browser window */
+    let server_addr = format!("http://{}/", server_addr);
+    if let Err(_) = webbrowser::open(&server_addr) {
+        log::warn!("Could not start browser");
+        log::info!("Please open this URL manually: {}", server_addr);
+    }
     /* attempt to complete the futures */
     tokio::select! {
         _ = &mut arena_task => {},
