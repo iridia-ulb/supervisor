527a2a9a1c6cf7ff96c6b6a46e586d08c64102d8
Michael Allwright <allsey87@gmail.com>
2021-03-10 17:02:02 +0100

    Merge associate and probe into single function and add timeouts

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,131 +1,92 @@
+use futures::stream::FuturesUnordered;
+use tokio::sync::mpsc;
+use tokio_stream::StreamExt;
+use std::{time::Duration, net::Ipv4Addr};
+
 pub mod xbee;
 pub mod fernbedienung;
 
-use futures::stream::FuturesUnordered;
-
-use tokio::{sync::mpsc, time::timeout};
-use tokio_stream::{StreamExt, wrappers::UnboundedReceiverStream};
-
-use std::{
-    time::Duration,
-    net::Ipv4Addr,
-};
-
 use crate::arena;
-
-pub enum Device {
-    Fernbedienung(fernbedienung::Device),
-    Xbee(xbee::Device),
-}
 
 #[derive(thiserror::Error, Debug)]
 enum Error {
-    #[error("Association timed out")]
-    Timeout,
-
-    #[error("Non-matching IP address")]
-    IpMismatch,
-
-    #[error(transparent)]
-    XbeeError(#[from] xbee::Error),
+    #[error("Could not associate {0} with a robot type")]
+    AssociateError(Ipv4Addr),
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
-pub async fn new(network_addr_rx: mpsc::UnboundedReceiver<Ipv4Addr>,
+pub async fn new(mut network_addr_rx: mpsc::UnboundedReceiver<Ipv4Addr>,
                  arena_request_tx: &mpsc::UnboundedSender<arena::Request>) {
-    let mut probe_queue : FuturesUnordered<_> = Default::default();
-    let mut addresses = UnboundedReceiverStream::new(network_addr_rx);
-
+    let mut associate_queue : FuturesUnordered<_> = Default::default();
     loop {
         tokio::select!{
-            Some(network_addr) = addresses.next() => {
-                probe_queue.push(probe(network_addr, None));
+            recv_addr = network_addr_rx.recv() => match recv_addr {
+                Some(addr) => {
+                    let assoc_attempt = associate(&arena_request_tx, addr, None);
+                    associate_queue.push(assoc_attempt);
+                },
+                /* terminate loop when the tx end of network_addr_rx is dropped */
+                None => break,
             },
-            Some((probe_addr, probe_result)) = probe_queue.next() => {
-                if let Ok(device) = probe_result {
-                    associate(device, &arena_request_tx).await;
-                }
-                else {
-                    /* TODO: perhaps match on different error types and delay accordingly */
-                    probe_queue.push(probe(probe_addr, Some(Duration::new(1,0))));
+            Some(associate_result) = associate_queue.next() => {
+                if let Err(Error::AssociateError(addr)) = associate_result {
+                    let delay = Duration::from_millis(1000);
+                    let assoc_attempt = associate(&arena_request_tx, addr, Some(delay));
+                    associate_queue.push(assoc_attempt);
                 }
             }
             else => break
         }
     }
 }
-    
-async fn associate(device: Device, arena_request_tx: &mpsc::UnboundedSender<arena::Request>) {
-    match device {
-        Device::Fernbedienung(device) => match device.hostname().await {
-            Ok(hostname) => {
-                match &hostname[..] {
-                    "ToshibaLaptop" => {
-                        if let Err(error) = arena_request_tx.send(arena::Request::AddPiPuck(device)) {
-                            log::error!("Could not add Pi-Puck to the arena: {}", error);
-                        }
-                    },
-                    "raspberrypi0-wifi" => {
-                        if let Err(error) = arena_request_tx.send(arena::Request::AddPiPuck(device)) {
-                            log::error!("Could not add Pi-Puck to the arena: {}", error);
-                        }
-                    },
-                    "upcore" => {
-                        if let Err(error) = arena_request_tx.send(arena::Request::PairWithDrone(device)) {
-                            log::error!("Could not pair drone to it's Xbee: {}", error);
-                        }
-                    },
-                    _ => log::warn!("Unrecognized fernbedienung device {} detected", hostname),
-                }
-            },
-            Err(error) => {
-                // the IP address should be returned to our pool here
-            }
-        },
-        Device::Xbee(device) => {
-            if let Err(error) = arena_request_tx.send(arena::Request::AddDrone(device)) {
-                log::error!("Could not add drone to the arena: {}", error);
-            }
-        }
+
+async fn associate(arena_request_tx: &mpsc::UnboundedSender<arena::Request>,
+                   addr: Ipv4Addr,
+                   delay: Option<Duration>) -> Result<()> {
+    /* optional sleep before attempting association */
+    if let Some(duration) = delay {
+        tokio::time::sleep(duration).await;
     }
-}
-
-
-
-async fn probe(addr: Ipv4Addr, delay: Option<Duration>) -> (Ipv4Addr, Result<Device>) {
-    /* wait delay before probing */
-    if let Some(delay) = delay {
-        tokio::time::sleep(delay).await;
-    }
-    /* attempt to connect to Xbee for 500 ms */
-    let assoc_xbee_attempt = timeout(Duration::from_millis(500), async {
+    /* assume address is an xbee and attempt to connect for 500 ms */
+    let xbee_attempt = tokio::time::timeout(Duration::from_millis(500), async {
         let device = xbee::Device::new(addr).await?;
-        /* validate the connection by checking if the remote IP address matches
-           the IP address that we connected to */
-        if addr == device.ip().await? {
-            Ok(device)
-        }
-        else {
-            Err(Error::IpMismatch)
-        }
-    });
-    if let Ok(assoc_xbee_result) = assoc_xbee_attempt.await {
-        /* TODO consider the Xbee error variant? */
-        if let Ok(device) = assoc_xbee_result {
-            return (addr, Ok(Device::Xbee(device)));
-        }
-    }
-    /* xbee connection timed out/failed */
-    /* attempt a fernbedienung connection for 500 ms */
-    let assoc_fernbedienung_attempt =
-        timeout(Duration::from_millis(500), fernbedienung::Device::new(addr));
-    if let Ok(assoc_fernbedienung_result) = assoc_fernbedienung_attempt.await {
-        if let Ok(device) = assoc_fernbedienung_result {
-            return (addr, Ok(Device::Fernbedienung(device)));
+        let addr = device.ip().await?;
+        std::result::Result::<_, xbee::Error>::Ok((addr, device))
+    }).await;
+    /* inspect result */
+    if let Ok(xbee_result) = xbee_attempt {
+        if let Ok((xbee_addr, xbee_device)) = xbee_result {
+            /* check if the remote address is the same as the address we are
+               attempting to connect to */
+            if xbee_addr == addr {
+                return arena_request_tx.send(arena::Request::AddDrone(xbee_device))
+                    .map_err(|_| Error::AssociateError(addr));
+            }
         }
     }
-    (addr, Err(Error::Timeout))
+    /* assume address is a device running the fernbedienung service and 
+       attempt to connect for 500 ms */
+    let fernbedienung_attempt = tokio::time::timeout(Duration::from_millis(500), async {
+        let device = fernbedienung::Device::new(addr).await?;
+        let hostname = device.hostname().await?;
+        std::result::Result::<_, fernbedienung::Error>::Ok((hostname, device))
+    }).await;
+    /* inspect result */
+    if let Ok(fernbedienung_result) = fernbedienung_attempt {
+        if let Ok((hostname, device)) = fernbedienung_result {
+            return match &hostname[..] {
+                "raspberrypi0-wifi" | "ToshibaLaptop" =>
+                    arena_request_tx.send(arena::Request::AddPiPuck(device))
+                        .map_err(|_| Error::AssociateError(addr)),
+                "upcore" =>
+                    arena_request_tx.send(arena::Request::PairWithDrone(device))
+                        .map_err(|_| Error::AssociateError(addr)),
+                _ => Err(Error::AssociateError(addr)),
+            };
+        }
+    }
+    Err(Error::AssociateError(addr))
 }
     
     
